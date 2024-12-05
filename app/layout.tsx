4600aab9a5b5cb46986a6d441388c14fba1ce44c
Regnars Klavins <regnars.klavins@gmail.com>
2024-12-05 09:45:03 +0200

    dark force fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,40 @@
+"use client";
+
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import { theme } from '@/theme';
-import "./Layout.css"
+import "./Layout.css";
 
 export const metadata = {
     title: 'Busu Vēstniecība',
     description: 'Visas jūsu kravas pārvadājumu vajadzības vienā vietā',
 };
 
-export default function RootLayout({ children }: { children: any }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    useEffect(() => {
+        // Ensure localStorage is set to dark mode
+        localStorage.setItem('mantine-color-scheme', 'dark');
+    }, []);
+
     return (
         <html lang="en" suppressHydrationWarning>
-            <head>
-                <ColorSchemeScript forceColorScheme={"dark"} />
-                <link rel="shortcut icon" href="/favicon.svg" />
-                <meta
-                    name="viewport"
-                    content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-                />
-            </head>
-            <body className="root"> {/* Optional: Add more scoped class */}
-                <MantineProvider  forceColorScheme={"dark"}>{children}</MantineProvider>
-            </body>
+        <head>
+            <ColorSchemeScript forceColorScheme="dark" />
+            <link rel="shortcut icon" href="/favicon.svg" />
+            <meta
+                name="viewport"
+                content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+            />
+        </head>
+        <body className="root">
+        <MantineProvider theme={theme} forceColorScheme="dark">
+            {children}
+        </MantineProvider>
+        </body>
         </html>
     );
 }
